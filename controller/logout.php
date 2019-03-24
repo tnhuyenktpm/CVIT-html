9ca6aec28ba64a52a4d8fbc80aaeda4bf2e5c92b
phuongle123 <32607417+phuongle123@users.noreply.github.com>
2019-03-24 11:18:16 +0700

    24/03/2019

--- controller/logout.php
+++
@@ -1,6 +0,0 @@
-<?php session_start(); 
-	if (isset($_SESSION['email'])){
-	    unset($_SESSION['email']); // xóa session login
-	    header ("Location:../views/index.php");
-	}
-?>
--- models/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-		$connect = mysqli_connect("localhost", "root", "", "do-an-3") or die("không thể kết nối được");
-		mysqli_query($connect,'SET NAMES UTF8');
-
-?>
